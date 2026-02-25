cd9567878f33f9c8e4b2a3dd0a3cbb42b461d93d
Wallace Rodrigues <wallace.rodriguesxt20@gmail.com>
2026-02-24 22:45:25 -0300

    add env file

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,13 +1,26 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-import { env } from '../config/env';
+function required(value: string | undefined, key: string) {
+  if (!value) throw new Error(`Variável ${key} não configurada.`);
+  return value;
+}
 
-export const supabase = createClient(env.supabaseUrl, env.supabaseAnonKey, {
+const supabaseUrl = required(
+  process.env.EXPO_PUBLIC_SUPABASE_URL,
+  'EXPO_PUBLIC_SUPABASE_URL'
+);
+
+const supabaseAnonKey = required(
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  'EXPO_PUBLIC_SUPABASE_ANON_KEY'
+);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false
-  }
+    detectSessionInUrl: false,
+  },
 });
